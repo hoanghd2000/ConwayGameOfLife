74d64cfaf1c4929ac39889d0abac2cd66e49bb24
hoanghd2000 <hoanghd2000@gmail.com>
2023-08-26 10:34:48 +0800

    Add range checking for InputGridSizeAction

--- Conway.Tests/InputGridSizeActionTests.cs
+++ Conway.Tests/InputGridSizeActionTests.cs
@@ -15,7 +15,7 @@ public class InputGridSizeActionTests
         action.Execute();
     
         // Assert
-        console.Received(1).WriteLine("Please enter grid size in w h format (example: 10 15):");
+        console.Received().WriteLine("Please enter grid size in w h format (example: 10 15):");
     }
 
     [Fact]
@@ -52,7 +52,7 @@ public class InputGridSizeActionTests
     [InlineData("")]
     [InlineData("1 7 5")]
     [InlineData("afsdf fsadf asdf 66")]
-    public void Should_Display_Error_When_User_Enter_Invalid_Number_Of_Word_Tokens(string input)
+    public void Should_Display_Error_When_User_Enters_Invalid_Number_Of_Word_Tokens(string input)
     {
         var console = Substitute.For<IConsoleFacade>();
         var action = new InputGridSizeAction(console);
@@ -60,14 +60,14 @@ public class InputGridSizeActionTests
         
         action.Execute();
         
-        console.Received(1).WriteLine("Wrong format for grid size!!! Please enter 2 integers in w h format (example: 10 15).");
+        console.Received(3).WriteLine("Wrong format for grid size!!!");
     }
 
     [Theory]
     [InlineData("ab cd")]
     [InlineData("8 cd")]
     [InlineData("cd 7")]
-    public void Should_Display_Error_When_User_Enter_Non_Numeric_Input(string input)
+    public void Should_Display_Error_When_User_Enters_Non_Numeric_Input(string input)
     {
         var console = Substitute.For<IConsoleFacade>();
         var action = new InputGridSizeAction(console);
@@ -75,6 +75,22 @@ public class InputGridSizeActionTests
         
         action.Execute();
         
-        console.Received(1).WriteLine("Width and Height must be numerical!!! Please enter 2 integers in w h format (example: 10 15).");
+        console.Received(3).WriteLine("Width and Height must be numerical!!!");
+    }
+
+    [Theory]
+    [InlineData("26 1")]
+    [InlineData("3 30")]
+    [InlineData("-1 0")]
+    public void Should_Display_Error_And_Prompt_Again_When_User_Enters_Out_Of_Range_Input(string input)
+    {
+        var console = Substitute.For<IConsoleFacade>();
+        var action = new InputGridSizeAction(console);
+        console.ReadLine().Returns(input);
+        
+        action.Execute();
+        
+        console.Received(3).WriteLine("Width and Height must be between 1 and 25 inclusive!!!");
+        console.Received(3).ReadLine();
     }
 }
--- Conway/InputGridSizeAction.cs
+++ Conway/InputGridSizeAction.cs
@@ -11,28 +11,41 @@ public class InputGridSizeAction : IAction
 
     public ActionResult Execute()
     {
-        Console.WriteLine("Please enter grid size in w h format (example: 10 15):");
+        var numTries = 3;
+        while (numTries > 0)
+        {
+            Console.WriteLine("Please enter grid size in w h format (example: 10 15):");
 
-        var line = Console.ReadLine();
-        var dimensions = line.Split();
+            var line = Console.ReadLine();
+            var dimensions = line.Split();
+            
+            if (dimensions.Length != 2)
+            {
+                Console.WriteLine("Wrong format for grid size!!!");
+                numTries -= 1;
+                continue;
+            }
+            
+            try
+            {
+                var width = int.Parse(dimensions[0]);
+                var height = int.Parse(dimensions[1]);
+                if (width < 1 || width > 25 || height < 1 || height > 25)
+                {
+                    Console.WriteLine("Width and Height must be between 1 and 25 inclusive!!!");
+                    numTries -= 1;
+                    continue;
+                }
+                return new ActionResult(new GameState(width, height, 0), new DisplayMenuAction(Console));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Width and Height must be numerical!!!");
+                numTries -= 1;
+            }
+        }
         
-        if (dimensions.Length != 2)
-        {
-            Console.WriteLine("Wrong format for grid size!!! Please enter 2 integers in w h format (example: 10 15).");
-            return new ActionResult(new GameState(0, 0, 0), new DisplayMenuAction(Console));
-        }
-
-        try
-        {
-            var width = int.Parse(dimensions[0]);
-            var height = int.Parse(dimensions[1]);
-            return new ActionResult(new GameState(width, height, 0), new DisplayMenuAction(Console));
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine("Width and Height must be numerical!!! Please enter 2 integers in w h format (example: 10 15).");
-            return new ActionResult(new GameState(0, 0, 0), new DisplayMenuAction(Console));
-        }
+        return new ActionResult(new GameState(0, 0, 0), new DisplayMenuAction(Console));
     }
 }
 
