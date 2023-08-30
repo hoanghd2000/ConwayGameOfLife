62b263541a9f7d117d2fc27a8957712542618c1f
hoanghd2000 <hoanghd2000@gmail.com>
2023-08-30 23:45:42 +0800

    Add RunAction

---
+++ Conway.Tests/RunActionTests.cs
@@ -0,0 +1,236 @@
+﻿using NSubstitute;
+
+namespace Conway.Tests;
+
+public class RunActionTests
+{
+    [Fact]
+    public void Should_Initialize_Board_From_GameState()
+    {
+        var console = Substitute.For<IConsoleFacade>();
+        var gameState = new GameState(5, 5, 3, 
+            new List<Cell> { new(0, 0), new(1, 1), new(1, 2) });
+        var action = new RunAction(console, gameState);
+
+        var board = action.InitializeBoard();
+        
+        Assert.True(board[0, 0]);
+        Assert.True(board[1, 1]);
+        Assert.True(board[2, 1]);
+        for (var i = 0; i < gameState.Height; i++)
+        {
+            for (var j = 0; j < gameState.Width; j++)
+            {
+                if (gameState.LiveCells.Contains(new Cell(j, i)))
+                {
+                    continue;
+                }
+                Assert.False(board[i, j]);
+            }
+        }
+    }
+
+    [Theory]
+    [InlineData(5, 5)]
+    [InlineData(0, 5)]
+    [InlineData(5, 3)]
+    [InlineData(-1, 3)]
+    [InlineData(3, -1)]
+    public void Should_Throw_Exception_When_Target_Cell_For_Live_Neighbours_Counting_Is_Out_Of_Range(int x, int y)
+    {
+        var console = Substitute.For<IConsoleFacade>();
+        var gameState = new GameState(5, 5, 3, new List<Cell> ());
+        var action = new RunAction(console, gameState);
+        
+        var board = action.InitializeBoard();
+        Assert.Throws<ApplicationException>(() => action.CountNumLiveNeighbors(board, x, y));
+    }
+
+    [Fact]
+    public void Should_Count_Number_Of_Live_Neighbours_For_Cell_In_A_1x1_Board()
+    {
+        var console = Substitute.For<IConsoleFacade>();
+        var gameState = new GameState(1, 1, 3, new List<Cell> { new(0, 0) });
+        var action = new RunAction(console, gameState);
+
+        var board = action.InitializeBoard();
+        var numLiveNeighbors = action.CountNumLiveNeighbors(board, 0, 0);
+        
+        Assert.Equal(0, numLiveNeighbors);
+    }
+
+    [Fact]
+    public void Should_Count_Number_Of_Live_Neighbours_For_Cell_In_A_1x2_Board()
+    {
+        var console = Substitute.For<IConsoleFacade>();
+        var gameState = new GameState(2, 1, 3, new List<Cell> { new(1, 0) });
+        var action = new RunAction(console, gameState);
+
+        var board = action.InitializeBoard();
+        var numLiveNeighbors = action.CountNumLiveNeighbors(board, 0, 0);
+        
+        Assert.Equal(1, numLiveNeighbors);
+    }
+    
+    [Fact]
+    public void Should_Count_Number_Of_Live_Neighbours_For_Centre_Cell_In_A_1x3_Board()
+    {
+        var console = Substitute.For<IConsoleFacade>();
+        var gameState = new GameState(3, 1, 3, new List<Cell> { new(0, 0), new(2, 0) });
+        var action = new RunAction(console, gameState);
+
+        var board = action.InitializeBoard();
+        var numLiveNeighbors = action.CountNumLiveNeighbors(board, 1, 0);
+        
+        Assert.Equal(2, numLiveNeighbors);
+    }
+    
+    [Theory]
+    [InlineData(0, 0)]
+    [InlineData(2, 0)]
+    public void Should_Count_Number_Of_Live_Neighbours_For_Edge_Cell_1_In_A_1x3_Board(int x, int y)
+    {
+        var console = Substitute.For<IConsoleFacade>();
+        var gameState = new GameState(3, 1, 3, new List<Cell> { new(0, 0), new(2, 0) });
+        var action = new RunAction(console, gameState);
+
+        var board = action.InitializeBoard();
+        var numLiveNeighbors = action.CountNumLiveNeighbors(board, x, y);
+        
+        Assert.Equal(1, numLiveNeighbors);
+    }
+    
+    [Fact]
+    public void Should_Count_Number_Of_Live_Neighbours_For_Edge_Cell_2_In_A_1x3_Board()
+    {
+        var console = Substitute.For<IConsoleFacade>();
+        var gameState = new GameState(3, 1, 3, new List<Cell> { new(0, 0), new(2, 0) });
+        var action = new RunAction(console, gameState);
+
+        var board = action.InitializeBoard();
+        var numLiveNeighbors = action.CountNumLiveNeighbors(board, 2, 0);
+        
+        Assert.Equal(1, numLiveNeighbors);
+    }
+    
+    [Fact]
+    public void Should_Count_Number_Of_Live_Neighbours_For_Cell_In_A_2x1_Board()
+    {
+        var console = Substitute.For<IConsoleFacade>();
+        var gameState = new GameState(1, 2, 3, new List<Cell> { new(0, 1), new(0, 0) });
+        var action = new RunAction(console, gameState);
+
+        var board = action.InitializeBoard();
+        var numLiveNeighbors = action.CountNumLiveNeighbors(board, 0, 0);
+        
+        Assert.Equal(1, numLiveNeighbors);
+    }
+    
+    [Fact]
+    public void Should_Count_Number_Of_Live_Neighbours_For_Centre_Cell_In_A_3x1_Board()
+    {
+        var console = Substitute.For<IConsoleFacade>();
+        var gameState = new GameState(1, 3, 3, new List<Cell> { new(0, 0), new(0, 2), new (0, 1) });
+        var action = new RunAction(console, gameState);
+
+        var board = action.InitializeBoard();
+        var numLiveNeighbors = action.CountNumLiveNeighbors(board, 0, 1);
+        
+        Assert.Equal(2, numLiveNeighbors);
+    }
+    
+    [Theory]
+    [InlineData(0, 0)]
+    [InlineData(0, 2)]
+    public void Should_Count_Number_Of_Live_Neighbours_For_Edge_Cell_In_A_3x1_Board(int x, int y)
+    {
+        var console = Substitute.For<IConsoleFacade>();
+        var gameState = new GameState(1, 3, 3, new List<Cell> { new(0, 0), new(0, 2), new (0, 1) });
+        var action = new RunAction(console, gameState);
+
+        var board = action.InitializeBoard();
+        var numLiveNeighbors = action.CountNumLiveNeighbors(board, x, y);
+        
+        Assert.Equal(2, numLiveNeighbors);
+    }
+
+    [Fact]
+    public void Should_Count_Number_Of_Live_Neighbours_For_A_Cell_In_A_Board_With_Both_Height_And_Width_Larger_Than_Or_Equal_To_3()
+    {
+        var console = Substitute.For<IConsoleFacade>();
+        var gameState = new GameState(3, 3, 3, new List<Cell> { new(1, 0), new(1, 1), new (1, 2), new(0, 1), new(2, 1) });
+        var action = new RunAction(console, gameState);
+
+        var board = action.InitializeBoard();
+        var numLiveNeighbors1 = action.CountNumLiveNeighbors(board, 1, 1);
+        var numLiveNeighbors2 = action.CountNumLiveNeighbors(board, 0, 1);
+        
+        Assert.Equal(4, numLiveNeighbors1);
+        Assert.Equal(4, numLiveNeighbors2);
+    }
+
+    [Fact]
+    public void Should_Ensure_That_LiveCell_With_Fewer_Than_2_Live_Neighbours_Dies()
+    {
+        var console = Substitute.For<IConsoleFacade>();
+        var gameState = new GameState(5, 5, 3, new List<Cell> { new(3, 0), new(4, 0), new (2, 2), new(3, 1) });
+        var action = new RunAction(console, gameState);
+
+        var board = action.InitializeBoard();
+        var resultBoard = action.ProcessOneIteration(board);
+        
+        Assert.False(resultBoard[2, 2]);
+    }
+    
+    [Fact]
+    public void Should_Ensure_That_LiveCell_With_2_Or_3_Live_Neighbours_Survives()
+    {
+        var console = Substitute.For<IConsoleFacade>();
+        var gameState = new GameState(5, 5, 3, new List<Cell> { new(3, 0), new(4, 0), new (2, 2), new(3, 1), new( 2, 0) });
+        var action = new RunAction(console, gameState);
+
+        var board = action.InitializeBoard();
+        var resultBoard = action.ProcessOneIteration(board);
+        
+        Assert.True(resultBoard[0, 3]);
+        Assert.True(resultBoard[0, 4]);
+    }
+    
+    [Fact]
+    public void Should_Ensure_That_LiveCell_With_More_Than_3_Live_Neighbours_Dies()
+    {
+        var console = Substitute.For<IConsoleFacade>();
+        var gameState = new GameState(5, 5, 3, new List<Cell> { new(3, 0), new(4, 0), new (2, 2), new(3, 1), new( 2, 0) });
+        var action = new RunAction(console, gameState);
+
+        var board = action.InitializeBoard();
+        var resultBoard = action.ProcessOneIteration(board);
+        
+        Assert.False(resultBoard[1, 3]);
+    }
+    
+    [Fact]
+    public void Should_Ensure_That_DeadCell_With_Exactly_3_Live_Neighbours_Becomes_LiveCell()
+    {
+        var console = Substitute.For<IConsoleFacade>();
+        var gameState = new GameState(5, 5, 3, new List<Cell> { new(3, 0), new(4, 0), new (2, 2), new(3, 1), new( 2, 0) });
+        var action = new RunAction(console, gameState);
+
+        var board = action.InitializeBoard();
+        var resultBoard = action.ProcessOneIteration(board);
+        
+        Assert.True(resultBoard[4, 3]);
+    }
+    
+    // [Fact]
+    // public void Should_Run_The_Number_Of_Generations_Specified()
+    // {
+    //     var console = Substitute.For<IConsoleFacade>();
+    //     var gameState = new GameState(5, 5, 3, new List<Cell> { new(3, 0), new(4, 0), new (2, 2), new(3, 1), new( 2, 0) });
+    //     var action = new RunAction(console, gameState);
+    //
+    //     var board = action.Execute();
+    //     
+    //     
+    // }
+}
---
+++ Conway/RunAction.cs
@@ -0,0 +1,178 @@
+﻿namespace Conway;
+
+public class RunAction : IAction
+{
+    private IConsoleFacade Console { get; }
+    private GameState CurrentGameState { get; }
+
+    public RunAction(IConsoleFacade console, GameState currentGameState)
+    {
+        Console = console;
+        CurrentGameState = currentGameState;
+    }
+
+    public ActionResult Execute()
+    {
+        var board = InitializeBoard();
+        Console.WriteLine("Initial position");
+        PrintBoard(board);
+        
+        var isTerminateChar = false;
+        for (var i = 0; i < CurrentGameState.NumGen; i++)
+        {
+            Console.WriteLine("Enter > to go to next generation or # to go back to main menu");
+            Console.WriteLine("");
+            
+            var isNextGenChar = false;
+            while (!isNextGenChar && !isTerminateChar)
+            {
+                var input = Console.ReadLine();
+                isNextGenChar = TryParseNextGenChar(input);
+                isTerminateChar = TryParseTerminatingChar(input);
+            }
+
+            if (isTerminateChar)
+            {
+                break;
+            }
+            
+            board = ProcessOneIteration(board);
+            Console.WriteLine($"Generation {i+1}");
+            PrintBoard(board);
+        }
+
+        if (!isTerminateChar)
+        {
+            Console.WriteLine("End of generation. Press any key to return to main menu");
+            Console.ReadLine();
+        }
+        
+        return new ActionResult(CurrentGameState, new DisplayMenuAction(Console, CurrentGameState));
+    }
+
+    public bool[, ] InitializeBoard()
+    {
+        var board = new bool[CurrentGameState.Height, CurrentGameState.Width];
+        foreach (var liveCell in CurrentGameState.LiveCells)
+        {
+            board[liveCell.Y, liveCell.X] = true;
+        }
+
+        return board;
+    }
+
+    public bool[, ] ProcessOneIteration(bool[, ] board)
+    {
+        var resultBoard = new bool[CurrentGameState.Height, CurrentGameState.Width];
+        for (var i = 0; i < CurrentGameState.Height; i++)
+        {
+            for (var j = 0; j < CurrentGameState.Width; j++)
+            {
+                var numLiveNeighbors = CountNumLiveNeighbors(board, j, i);
+                if (board[i, j])
+                {
+                    if (numLiveNeighbors is 2 or 3)
+                    {
+                        resultBoard[i, j] = true;
+                    }
+                    else
+                    {
+                        resultBoard[i, j] = false;
+                    }
+                }
+                else
+                {
+                    if (numLiveNeighbors == 3)
+                    {
+                        resultBoard[i, j] = true;
+                    }
+                    else
+                    {
+                        resultBoard[i, j] = false;
+                    }
+                }
+            }
+        }
+
+        return resultBoard;
+    }
+
+    public int CountNumLiveNeighbors(bool[, ] board, int x, int y)
+    {
+        if (x < 0 || x >= CurrentGameState.Width || y < 0 || y >= CurrentGameState.Height)
+        {
+            throw new ApplicationException("Cell coordinates out of range!");
+        }
+
+        var numLiveNeighbors = 0;
+
+        var xLeftBound = x - 1;
+        var xRightBound = x + 1;
+        var yLeftBound = y - 1;
+        var yRightBound = y + 1;
+
+        if (CurrentGameState.Width < 3)
+        {
+            xLeftBound = 0;
+            xRightBound = CurrentGameState.Width - 1;
+        }
+        
+        if (CurrentGameState.Height < 3)
+        {
+            yLeftBound = 0;
+            yRightBound = CurrentGameState.Height - 1;
+        }
+
+        for (var i = xLeftBound; i <= xRightBound; i++)
+        {
+            for (var j = yLeftBound; j <= yRightBound; j++)
+            {
+                if (i == x && j == y)
+                {
+                    continue;
+                }
+                
+                if (board[(j + CurrentGameState.Height) % CurrentGameState.Height, (i + CurrentGameState.Width) % CurrentGameState.Width])
+                {
+                    numLiveNeighbors++;
+                }
+            }
+        }
+
+        return numLiveNeighbors;
+    }
+
+    private void PrintBoard(bool[,] board)
+    {
+        for (var i = 0; i < board.GetLength(0); i++)
+        {
+            for (var j = 0; j < board.GetLength(1); j++)
+            {
+                System.Console.Write(board[i, j] ? "o" : ".");
+            }
+            System.Console.WriteLine();
+        }
+    }
+
+    private bool TryParseNextGenChar(string input)
+    {
+        var inputTokens = input.Split();
+        if (inputTokens.Length != 1)
+        {
+            return false;
+        }
+
+        return inputTokens[0] == ">";
+    }
+    
+    private bool TryParseTerminatingChar(string input)
+    {
+        var inputTokens = input.Split();
+        if (inputTokens.Length != 1)
+        {
+            return false;
+        }
+
+        return inputTokens[0] == "#";
+    }
+}
